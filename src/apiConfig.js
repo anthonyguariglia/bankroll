e5c5d401c4a7d7deb04cf8e4e0389052a3d99193
anthonyguariglia <aguariglia197@gmail.com>
2022-02-11 17:33:05 -0500

    update heroku api link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://fathomless-wave-79069.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
